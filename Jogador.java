3e8b140a25ef1c03f7d2d3b98ddb1c2e40c88668
FabianoCosta555 <41345942+FabianoCosta555@users.noreply.github.com>
2019-09-11 15:42:58 -0300

    Arquivos Java
    Arquivo principal "JogoDaVelha.java".

---
+++ Jogador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogodavelha;
+
+import jogodavelha.design.TelaInicialJogo;
+
+/**
+ *
+ * @author Fabiano
+ */
+public class Jogador extends TelaInicialJogo {
+    
+    
+    public Jogador(){
+                
+    
+    }
+    
+    String nome=null;
+   // String nome02=null;
+    String simbolo=null;
+   // String simbolo02=null;
+    
+}
---
+++ Partida.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogodavelha;
+
+/**
+ *
+ * @author Fabiano
+ */
+public class Partida {
+    
+    int numerodePartida=0;
+    int dataPartida=0;
+    int pontosNegaJog1=0;
+    int pontosNegaJog2=0;
+    String jogador1="";
+    String jogador2="";
+    String ganhadorNega="";
+    
+    
+    public void validaDados(){
+    
+    }
+    
+    public void novaPartida(){
+    
+    }
+    
+    
+    
+}
